6ece279fc1c7079b8462e93e961b62bcd82023ff
Duvan Reyes <duvan.reyesgonzalez@unicolombo.edu.co>
2024-04-21 19:10:10 -0500

    SUNDAY

---
+++ sCROOLL/src/main/java/guii10/SCROOLL.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package guii10;
+
+import guii10.Scrollbar;
+import javax.swing.JFrame;
+
+public class SCROOLL extends JFrame{
+
+    public static void main(String[] args) {
+        
+        Scrollbar barra= new Scrollbar();
+        barra.setVisible(true);
+    }
+  
+}
